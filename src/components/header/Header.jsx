28c3e27d7c29ba0b724a7ca5fa0cc22bfb9bd22c
101ALEKSANDER101 <lx2006861@gmail.com>
2024-02-04 18:56:41 +0300

    change 4

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
 	return (
 		<div className={'header'}>
 			<Button onClick={onClose}>Закрыть</Button>
-			<span className={'username'}>{user?.userName}</span>
+			<span className={'username'}>{user?.username}</span>
 		</div>
 	);
 }
